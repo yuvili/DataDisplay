f54b469c7143541a9d8439429283af0d86d1e416
yuvili <yuvalsh97@gmail.com>
2022-05-02 20:40:36 +0300

    gsp type update

--- LogTable.py
+++ LogTable.py
@@ -1,31 +1,107 @@
 import json
-from types.Anchor import cognataEngine
+
+from types.ActionScript import action_script
+from types.Anchor import anchor
+from types.GPS import gps
+from types.MovingObjectPath import moving_object_path
+from types.SensorConfiguration import sensor_configuration
+from types.VehicleInfo import vehicle_info
 
 
 class logTable:
 
-    def __init__(self, logs):
+    def __init__(self, logs, latitudes, longitudes):
         self.log = logs
+        self.latitudes = latitudes
+        self.longitudes = longitudes
 
     def load_from_json(self, file_name: str) -> bool:
         flag = True
         try:
             with open(file_name, "r") as f:
                 logs = []
+                longitudes = []
+                latitudes = []
                 lst = json.load(f)
                 logs = lst["Logs"]
 
                 for v in logs:
                     t = v["Type"]
-                    word_time = v["WorldTime"]
-                    simulation_time = v["SimulationTime"]
-                    frame_id = v["FrameID"]
-                    anchors = v["Anchors"]
 
-                    ce = cognataEngine(t, word_time, simulation_time, frame_id, anchors)
-                    logs.append(ce)
+                    match t:
+                        case "GPS":
+                            gpss = gps(t, v["WorldTime"], v["SimulationTime"], v["FrameID"], v["SimulationPosition"],
+                                       v["NoisedSimulationPosition"],v["Latitude"], v["Longitude"], v["Altitude"],
+                                       v["NoisedLatitude"], v["NoisedLongitude"],
+                                       v["NoisedAltitude"], v["Orientation"], v["Speed"], v["Acceleration"],
+                                       v["PositionInLane"],
+                                       v["LaneWidth"], v["LaneNumber"], v["NavigationSegment"], v["VelocityLocal3D"],
+                                       v["AngularVelocityLocal3D"], v["AngularAccelerationLocal3D"],
+                                       v["InstanceNumber"],
+                                       v["OnRoadDirection"], v["RoadSpeedLimit"], v["HeadedTrafficLightDistance"],
+                                       v["TargetDistance"])
+                            longitudes.append(v["Longitude"])
+                            latitudes.append(v["Latitude"])
+                            logs.append(gpss)
 
-                self.__init__(logs)
+                        case "Anchor":
+                            word_time = v["WorldTime"]
+                            simulation_time = v["SimulationTime"]
+                            frame_id = v["FrameID"]
+                            anchors = v["Anchors"]
+                            an = anchor(t, word_time, simulation_time, frame_id, anchors)
+                            logs.append(an)
+
+                        case "ActionScript":
+                            word_time = v["WorldTime"]
+                            simulation_time = v["SimulationTime"]
+                            frame_id = v["FrameID"]
+                            actor_name = v["ActorName"]
+                            trigger = v["Trigger"]
+                            actions = v["Actions"]
+                            asc = action_script(t, word_time, simulation_time, frame_id, actor_name, trigger, actions)
+                            logs.append(asc)
+
+                        case "SensorConfiguration":
+                            word_time = v["WorldTime"]
+                            simulation_time = v["SimulationTime"]
+                            frame_id = v["FrameID"]
+                            sensor_type = v["SensorType"]
+                            sensor_name = v["SensorName"]
+                            pos = v["Position"]
+                            rotation = v["Rotation"]
+                            projection_matrix = v["ProjectionMatrix"]
+                            near = v["Near"]
+                            far = v["Far"]
+                            fov = v["FOV"]
+                            width = v["Width"]
+                            height = v["Height"]
+                            aspect_ratio = v["AspectRatio"]
+                            output_quality = v["OutputQualit"]
+                            sc = sensor_configuration(t, word_time, simulation_time, frame_id, sensor_type, sensor_name,
+                                                      pos, rotation, projection_matrix, near, far, fov, width, height,
+                                                      aspect_ratio, output_quality)
+                            logs.append(sc)
+
+                        case "VehicleInfo":
+                            word_time = v["WorldTime"]
+                            simulation_time = v["SimulationTime"]
+                            frame_id = v["FrameID"]
+                            name = v["Name"]
+                            dimensions = v["Dimensions"]
+                            vi = vehicle_info(t, word_time, simulation_time, frame_id, name, dimensions)
+                            logs.append(vi)
+
+                        case "MovingObject Path":
+                            word_time = v["WorldTime"]
+                            simulation_time = v["SimulationTime"]
+                            frame_id = v["FrameID"]
+                            vehicle_name = v["VehicleName"]
+                            waypoints = v["Waypoints"]
+                            mop = moving_object_path(t, word_time, simulation_time, frame_id, vehicle_name, waypoints)
+                            logs.append(mop)
+
+                self.__init__(logs, latitudes, longitudes)
         except FileNotFoundError:
             flag = False
             raise FileNotFoundError
---
+++ types/GPS.py
@@ -0,0 +1,66 @@
+from position import Position
+
+
+class gps:
+
+    # {"Type":"GPS",
+    # "WorldTime":"12:58:50.9982520",
+    # "SimulationTime":0.0666666701436043,
+    # "FrameID":2,
+    # "SimulationPosition":{"x":"132.907","y":"77.722","z":"1.817"},
+    # "NoisedSimulationPosition":{"x":"132.907","y":"77.722","z":"1.817"},
+    # "Latitude":41.740229749826,
+    # "Longitude":-116.946502320116,
+    # "Altitude":1.8171272277832,
+    # "NoisedLatitude":41.740229749826,
+    # "NoisedLongitude":-116.946502320116,
+    # "NoisedAltitude":1.8171272277832,
+    # "Orientation":{"x":"0.037","y":"0.100","z":"0.147"},
+    # "Speed":-0.000369598652468994,
+    # "Acceleration":{"x":"-0.001","y":"-0.006","z":"1.150"},
+    # "PositionInLane":2.40606386796571E-05,
+    # "LaneWidth":3.5,
+    # "LaneNumber":-1,
+    # "NavigationSegment":30,
+    # "VelocityLocal3D":{"x":"0.000","y":"-0.002","z":"0.365"},
+    # "AngularVelocityLocal3D":{"x":"0.014","y":"0.037","z":"0.000"},
+    # "AngularAccelerationLocal3D":{"x":"0.052","y":"0.150","z":"0.000"},
+    # "InstanceNumber":176,
+    # "OnRoadDirection":true,
+    # "RoadSpeedLimit":40.2336006164551,
+    # "HeadedTrafficLightDistance":10000,
+    # "TargetDistance":10000}
+    def __init__(self, t: str, world_time: str, simulation_time: float, frame_id: int, simulation_pos: Position,
+                 noised_simulation_position: Position, latitude: float, longitude: float, altitude: float,
+                 noised_latitude: float, noised_longitude: float, noised_altitude: float, orientation: Position,
+                 speed: float, acceleration: Position,
+                 pos_in_lane: int, lane_width: float, lane_number: int, navi_segment: int, velocity_local3D: Position,
+                 angular_velocity_local3D: Position, angular_acceleration_local3D: Position, instance_num: int,
+                 on_road_direction: bool, road_speed_limit: float, headed_traffic_light_distance:int, target_distance: int):
+        self.typ = t
+        self.world_time = world_time
+        self.simulation_time = simulation_time
+        self.noised_simulation_position = noised_simulation_position
+        self.frame_id = frame_id
+        self.simulation_pos = simulation_pos
+        self.latitude = latitude
+        self.longitude = longitude
+        self.altitude = altitude
+        self.noised_latitude = noised_latitude
+        self.noised_longitude = noised_longitude
+        self.noised_altitude = noised_altitude
+        self.orientation = orientation
+        self.speed = speed
+        self.acceleration = acceleration
+        self.pos_in_lane = pos_in_lane
+        self.lane_width = lane_width
+        self.lane_number = lane_number
+        self.navi_segment = navi_segment
+        self.velocity_local3D = velocity_local3D
+        self.angular_velocity_local3D = angular_velocity_local3D
+        self.angular_acceleration_local3D = angular_acceleration_local3D
+        self.instance_num = instance_num
+        self.on_road_direction = on_road_direction
+        self.road_speed_limit = road_speed_limit
+        self.headed_traffic_light_distance = headed_traffic_light_distance
+        self.target_distance = target_distance
