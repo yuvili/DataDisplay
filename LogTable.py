255c76d72544f733ee1175ef1e86c5fabdc0b53d
yuvili <yuvalsh97@gmail.com>
2022-07-18 11:00:16 +0300

    final

--- LogTable.py
+++ LogTable.py
@@ -1,5 +1,4 @@
 import json
-
 from GPS import gps
 
 
@@ -11,42 +10,34 @@ class logTable:
         self.longitudes = []
         self.simulation_time = []
 
-    def load_from_json(self, file_name: str) -> bool:
-        flag = True
-        try:
-            with open(file_name, "r") as f:
-                logs = []
-                longitudes = []
-                latitudes = []
-                simulation_times = []
-                lst = json.load(f)
+    def load_from_json(self, file_name: str):
+        with open(file_name, "r") as f:
+            logs = []
+            longitudes = []
+            latitudes = []
+            simulation_times = []
+            lst = json.load(f)
 
-                for v in lst["Logs"]:
-                    t = v["Type"]
+            for v in lst["Logs"]:
+                t = v["Type"]
 
-                    match t:
-                        case "GPS":
-                            gpss = gps(t, v["WorldTime"], v["SimulationTime"], v["FrameID"],v["SimulationPosition"],
-                                       v["NoisedSimulationPosition"], v["Latitude"], v["Longitude"], v["Altitude"],
-                                       v["NoisedLatitude"], v["NoisedLongitude"],
-                                       v["NoisedAltitude"], v["Orientation"], v["Speed"], v["Acceleration"],
-                                       v["PositionInLane"],
-                                       v["LaneWidth"], v["LaneNumber"], v["NavigationSegment"], v["VelocityLocal3D"],
-                                       v["AngularVelocityLocal3D"], v["AngularAccelerationLocal3D"],
-                                       v["InstanceNumber"],
-                                       v["OnRoadDirection"], v["RoadSpeedLimit"], v["HeadedTrafficLightDistance"],
-                                       v["TargetDistance"])
-                            longitudes.append(v["Longitude"])
-                            latitudes.append(v["Latitude"])
-                            simulation_times.append(v["SimulationTime"])
-                            logs.append(gpss)
+                if t == "GPS":
+                    gpss = gps(t, v["WorldTime"], v["SimulationTime"], v["FrameID"], v["SimulationPosition"],
+                               v["NoisedSimulationPosition"], v["Latitude"], v["Longitude"], v["Altitude"],
+                               v["NoisedLatitude"], v["NoisedLongitude"],
+                               v["NoisedAltitude"], v["Orientation"], v["Speed"], v["Acceleration"],
+                               v["PositionInLane"],
+                               v["LaneWidth"], v["LaneNumber"], v["NavigationSegment"], v["VelocityLocal3D"],
+                               v["AngularVelocityLocal3D"], v["AngularAccelerationLocal3D"],
+                               v["InstanceNumber"],
+                               v["OnRoadDirection"], v["RoadSpeedLimit"], v["HeadedTrafficLightDistance"],
+                               v["TargetDistance"])
+                    longitudes.append(v["Longitude"])
+                    latitudes.append(v["Latitude"])
+                    simulation_times.append(v["SimulationTime"])
+                    logs.append(gpss)
 
-                self.log = logs
-                self.latitudes = latitudes
-                self.longitudes = longitudes
-                self.simulation_time = simulation_times
-        except FileNotFoundError:
-            flag = False
-            raise FileNotFoundError
-        finally:
-            return flag
+            self.log = logs
+            self.latitudes = latitudes
+            self.longitudes = longitudes
+            self.simulation_time = simulation_times
--- display.py
+++ display.py
@@ -2,14 +2,17 @@ from LogTable import logTable
 from matplotlib import pyplot as plt
 import pandas as pd
 
-close = pd.read_excel(r"g.techAmp Data of A1_090851_LATENCY_2_close at (13;01;27).xlsx", index_col=1)
-close_data = pd.DataFrame(close, columns=['GSR'])
-far = pd.read_excel(r"g.techAmp Data of A1_090851_Slalom_1_far at (13;05;9).xlsx", index_col=1)
-far_data = pd.DataFrame(far, columns=['GSR'])
+# GSR Data from given excel files
+close = pd.read_excel(r"g.techAmp Data of A1_090851_LATENCY_2_close at (13;01;27).xlsx")
+far = pd.read_excel(r"g.techAmp Data of A1_090851_Slalom_1_far at (13;05;9).xlsx")
+
+# Data Frame
+close_data = pd.DataFrame(close, columns=["GSR"])
+far_data = pd.DataFrame(far, columns=["GSR"])
 
 
 def displays(json_file_name_far: str, json_file_name_close: str):
-    # far
+    # ----- Far Data Frame -----
     plt.style.use('seaborn')
     plt.title("Slalom Simulation - Far")
     lt_far = logTable()
@@ -17,13 +20,14 @@ def displays(json_file_name_far: str, json_file_name_close: str):
 
     for lap in lt_far.log:
         simul_time = lap.simulation_time
-        plt.plot(lap.latitude, lap.longitude, marker='o', linestyle='', markersize=10, label=far_data['GSR'].get(simul_time))
+        plt.plot(lap.latitude, lap.longitude, marker='o', linestyle='', markersize=10,
+                 label=far_data['GSR'].get(simul_time))
 
     plt.xlabel('Latitude')
     plt.ylabel('Longitude')
     plt.show()
 
-    # close
+    # ----- Close Data Frame -----
     plt.title("Slalom Simulation - Close")
     lt_close = logTable()
     lt_close.load_from_json(json_file_name_close)
@@ -38,5 +42,4 @@ def displays(json_file_name_far: str, json_file_name_close: str):
     plt.show()
 
 
-if __name__ == '__main__':
-    displays("CognataEngineLog_far.json", "CognataEngineLog_close.json")
+displays("CognataEngineLog_far.json", "CognataEngineLog_close.json")
